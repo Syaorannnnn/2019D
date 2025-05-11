89d055a737fd31b6c7658e1a4c8c1ec37e40bf94
lingdajin <Ling_da_jin@bupt.edu.cn>
2025-05-11 21:01:16 +0800

    2025-5-11

--- USER/UART/UART.h
+++ USER/UART/UART.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include "ti_msp_dl_config.h"
 
-void sendData(uint8_t data);
-void sendString(char* p);
-void sendNum(float num, int decimalPlaces);
+void sendData(uint8_t data, UART_Regs *huart);
+void sendString(char *p, UART_Regs *huart);
+void sendNum(float num, int decimalPlaces, UART_Regs *huart);
 
 #endif
